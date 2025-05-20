eadd05141b451b810599a917abd186daf206108e
20243ds064@utez.edu.mx <20243ds064@utez.edu.mx>
2025-05-20 09:54:05 -0600

    Crecion del script de promedio

---
+++ numeros_promedio/numerosPromedio/promedioNum.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+public class promedioNum{
+    public static void main(String[] args) throws Exception {
+        String x1 = JOptionPane.showInputDialog("Ingrese el primer número");
+        String x2 = JOptionPane.showInputDialog("Ingrese el segundo número");
+        String x3 = JOptionPane.showInputDialog("Ingrese el tercer número");
+        //System.out.println((Integer.parseInt(x1) + Integer.parseInt(x2) + Integer.parseInt(x3)) / 3);
+    }
+}
--- numeros_promedio/src/App.java
+++ numeros_promedio/src/App.java
@@ -1,5 +1,12 @@
+import javax.swing.JOptionPane;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       
+       String x1 = JOptionPane.showInputDialog("Dame el primer numero");
+       String x2 = JOptionPane.showInputDialog("Dame segundo numero");
+       String x3 = JOptionPane.showInputDialog("Dame el tercer numero");
+    
+       System.out.println((Integer.parseInt(x1) + Integer.parseInt(x2) + Integer.parseInt(x3)) / 3);
     }
 }
