acd4c732842d8f463497f72f0587ed7440a45a22
20243ds064@utez.edu.mx <20243ds064@utez.edu.mx>
2025-05-22 18:34:52 -0600

    creamos el main donde mandaremos a traer las funciones de areas y perimetros

---
+++ areasPerimetros/src/Model/Main.java
@@ -0,0 +1,12 @@
+package Model;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        //System.out.println("Hello, World!");
+
+        square sq = new square();
+        sq.setLado(10);
+        sq.setMessage("");
+
+
+    }
+}
