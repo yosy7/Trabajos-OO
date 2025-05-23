50dae628022cbb7df51598232089f58c0ed742c7
20243ds064@utez.edu.mx <20243ds064@utez.edu.mx>
2025-05-22 19:49:11 -0600

    crear calculo del area y perimetro del triangulo

--- areaPerimetro/src/Model/Triangle.java
+++ areaPerimetro/src/Model/Triangle.java
@@ -1,5 +1,37 @@
 package Model;
 
 public class Triangle {
+    private double ladoTriangulo;
+
+
+    private double baseTriangulo;
+    private double alturaTriangulo;
+
+    public double getAreaTriangle() {
+        return (baseTriangulo * alturaTriangulo) / 2;
+    }
+    public void setBaseTriangulo(double baseTriangulo) {
+        this.baseTriangulo = baseTriangulo;
+    }
+    public double getBaseTriangulo() {
+        return this.baseTriangulo;
+    }
+    public void setAlturaTriangulo(double alturaTriangulo) {
+        this.alturaTriangulo = alturaTriangulo;
+    }
+    public double getAlturaTriangulo() {
+        return this.alturaTriangulo;
+    }
+
+    public void setLadoTriangulo(double ladoTriangulo) {
+        this.ladoTriangulo = ladoTriangulo;
+    }
+    public double getLadoTriangulo() {
+        return this.ladoTriangulo;
+    }
+
+    public double getPerimeterTriangle() {
+        return 2 *  ladoTriangulo + baseTriangulo;
+    }
 
 }
