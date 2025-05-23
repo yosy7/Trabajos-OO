37311afa8291828fef7bb348ea9e30147da2252a
20243ds064@utez.edu.mx <20243ds064@utez.edu.mx>
2025-05-22 19:56:14 -0600

    se agrega los valores para dar el resulatdo del area y perimetro del triangulo

--- areaPerimetro/src/Model/Main.java
+++ areaPerimetro/src/Model/Main.java
@@ -24,6 +24,18 @@ public class Main {
         System.out.println("radio: " + c.getRadio());
         System.out.println("area: " + c.getAreaCircle());
         System.out.println("Perimetro: " + c.getPerimeterCircle());
+
+        System.out.println();
+        System.out.println("TRIANGLE------------------------");
+        Triangle t = new Triangle();
+        t.setBaseTriangulo(10);
+        t.setAlturaTriangulo(15);
+        t.setLadoTriangulo(18);
+        System.out.println("Base: " +  t.getBaseTriangulo());
+        System.out.println("Altura: " +  t.getAlturaTriangulo());
+        System.out.println("Lado: " +  t.getLadoTriangulo());
+        System.out.println("Area: " + t.getAreaTriangle());
+        System.out.println("Perimetro: " + t.getPerimeterTriangle());
     }
 
 }
