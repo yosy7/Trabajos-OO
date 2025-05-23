bd3b7a623caf2e6babb494f5f9300a061d7c5f71
20243ds064@utez.edu.mx <20243ds064@utez.edu.mx>
2025-05-22 19:23:16 -0600

    crea clase square

--- areaPerimetro/src/Model/Main.java
+++ areaPerimetro/src/Model/Main.java
@@ -1,5 +1,19 @@
 package Model;
 
 public class Main {
+    public static void main(String[] args) throws Exception {
+        //System.out.println("Hello, World!");
+        square sq = new square();
+        sq.setLado(10);
+        sq.setMessage("");
+
+        System.out.println(sq.getLado());
+        
+        System.out.println(sq.getPerimeter());
+        sq.getPerimeter();
+        System.out.println(sq.getArea());
+        System.out.println();
+        sq.getArea();
+    }
 
 }
--- areaPerimetro/src/Model/square.java
+++ areaPerimetro/src/Model/square.java
@@ -1,5 +1,35 @@
 package Model;
 
 public class square {
+    private double lado;
+    private String message;
 
+    public double getPerimeter() {
+            //double perimeter=lado * 4;
+            //return perimeter;
+               
+        return lado * 4;
+        
+    }
+    public double getArea() {
+        if(!this.message.isEmpty()){
+        getSecretMessage();
+    }
+        return lado * lado;
+
+    }
+    public void setLado(double lado) {
+        this.lado = lado;
+    }
+    public double getLado() {
+        return this.lado;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+     private void getSecretMessage() {
+        System.out.println("este metodo solo es accesible desde la clase square, pero no desde afuera");
+    }
 }
