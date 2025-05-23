23a6a97e55328dc72405057780b01ea2a2f8df40
20243ds064@utez.edu.mx <20243ds064@utez.edu.mx>
2025-05-22 19:34:25 -0600

    realizar el calculo-operaciones del area y perimetro del circulo

--- areaPerimetro/src/Model/circle.java
+++ areaPerimetro/src/Model/circle.java
@@ -1,5 +1,20 @@
 package Model;
 
 public class circle {
+private double radio;
 
+ public double getAreaCircle() {
+        return 3.1416 * radio * radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+    public double getRadio() {
+        return this.radio;
+    }
+
+    public double getPerimeterCircle() {
+        return 2 * 3.1416 * radio;
+    }
 }
