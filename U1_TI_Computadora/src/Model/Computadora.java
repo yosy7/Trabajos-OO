8c0ce9afffeb38b20edfb85036751b0983dbd0f4
20243ds064@utez.edu.mx <20243ds064@utez.edu.mx>
2025-05-26 09:54:42 -0600

    se crea el script

--- U1_TI_Computadora/src/Model/Computadora.java
+++ U1_TI_Computadora/src/Model/Computadora.java
@@ -1,5 +1,51 @@
 package Model;
 
 public class Computadora {
-  
+  private String marca ;
+  private String modelo;
+  private boolean encendida;
+
+  public Computadora(String marca, String modelo){
+    this.marca = "HP";
+    this.modelo = modelo;
+    this.encendida = false;
+  }
+
+  public void encender(){
+    if (encendida){
+      
+      encendida = true;
+    }else {
+        System.out.println("Encendiendo...");
+        encendida = true;
+    }
+  }
+
+  public void apagar(){
+    if (encendida){
+        encendida = false;
+        System.out.println("Apagando");
+    }else{
+        
+        encendida=false;
+    }
+  }
+
+  public String getMarca() {
+    return marca;
+  }
+
+  public String getModelo() {
+    return modelo;
+  }
+
+  public boolean isEncendida() {
+    return encendida;
+  }
+
+  public void setModelo(String modelo) {
+    this.modelo = modelo;
+  }
+
+
 }
--- U1_TI_Computadora/src/Model/Main.java
+++ U1_TI_Computadora/src/Model/Main.java
@@ -1,5 +1,17 @@
 package Model;
 
 public class Main {
+      public static void main(String [] args)throws Exception{
+        //Computadora computer = new Computadora();
+        Computadora computer = new Computadora("", "");
+        computer.setModelo("OMEN");
+        
+        
+        System.out.println("marca: "+ computer.getMarca());
+        System.out.println("modelo: "+ computer.getModelo());
+        computer.encender();
+        computer.encender();
+        computer.apagar();
 
+      }
 }
