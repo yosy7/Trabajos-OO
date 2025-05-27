9bd28c04ab737decfe380f6d1994d61d977d9805
20243ds064@utez.edu.mx <20243ds064@utez.edu.mx>
2025-05-27 09:38:28 -0600

    se crea el script de CuentaBancaria

--- U1_TI_03_CuentaBancariaSimple/src/Model/CuentaBancaria.java
+++ U1_TI_03_CuentaBancariaSimple/src/Model/CuentaBancaria.java
@@ -1,5 +1,49 @@
 package Model;
 
 public class CuentaBancaria {
+  private String titular;
+  private double saldo;
 
+  public CuentaBancaria(String titular){
+     this.titular = titular;
+  }
+  
+  //public CuentaBancaria(Double saldo){
+   // this.saldo = saldo;
+  //}
+  
+  public void depositar(double monto){
+     if(monto > 0){
+        saldo += monto;
+        System.out.println("Deposito exitoso de:" + monto);
+        //System.out.println("Saldo: " + saldo);
+     }else{
+        System.out.println("Deposito fallido");
+     }
+  }
+
+  public boolean retirar(double monto){
+    if(monto > 0 && saldo >= monto){
+        saldo -= monto;
+
+        System.out.println("Retiro de " + monto);
+        return true;
+    }else{
+        System.out.println("retiro fallido");
+        return false;
+        
+    }
+  }
+  public String getTitular() {
+    return titular;
+  }
+  public Double getSaldo() {
+    return saldo;
+  }
+  public void setTitular(String titular) {
+    this.titular = titular;
+  }
+
+  
+  
 }
--- U1_TI_03_CuentaBancariaSimple/src/Model/Main.java
+++ U1_TI_03_CuentaBancariaSimple/src/Model/Main.java
@@ -1,5 +1,22 @@
 package Model;
 
 public class Main {
+    public static void main(String[] args) throws Exception {
+        CuentaBancaria cuenta = new CuentaBancaria("yoselinne");
+        //cuenta.setTitular("Yoselinne");
+        
+        System.out.println("CLIENTE: " + cuenta.getTitular());
+        cuenta.depositar(1000);
+         System.out.println("Saldo: " + cuenta.getSaldo());
+         cuenta.retirar(200);
+         System.out.println("Saldo: " + cuenta.getSaldo());
+         cuenta.retirar(10000);
+        System.out.println("Saldo: " + cuenta.getSaldo());
+
+        
+        
+
+
+    }
 
 }
