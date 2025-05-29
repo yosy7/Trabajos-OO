729cc45247d8e86819c5bacad36c51b2e4d98963
20243ds064@utez.edu.mx <20243ds064@utez.edu.mx>
2025-05-29 08:21:18 -0600

    ...

--- U1_TI_05_UsusarioContraseñaSegura/src/App.java
+++ U1_TI_05_UsusarioContraseñaSegura/src/App.java
@@ -1,5 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        System.out.println(Character.isUpperCase('a'));
+
+        String test = "Hola";
+        for(int i = 0; i< test.length(); i++){
+           System.out.println(test.charAt(i));
+        }
     }
+    
+    
+    
+
+    
 }
--- U1_TI_05_UsusarioContraseñaSegura/src/Model/UsuarioSeguro.java
+++ U1_TI_05_UsusarioContraseñaSegura/src/Model/UsuarioSeguro.java
@@ -1,5 +1,25 @@
 package Model;
 
 public class UsuarioSeguro {
+    private String nombreUsuario;
+    private String password;
 
-}
+    public void setNombreUsuario(String nombreUsuario ){
+        if(nombreUsuario== null){
+           System.out.println("el usuario no puede estar vacio");
+        }
+      this.nombreUsuario = nombreUsuario; 
+    }
+
+    public void setPassword(String password){
+        if(password.length() < 8 || password == null){
+           System.out.println("la contraseña debe tener al menos 8  caracteres");
+        } 
+        
+
+        }
+    }
+
+
+
+
