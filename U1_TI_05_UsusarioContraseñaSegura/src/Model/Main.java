c6793657bf2e4d0631adccb15bdc7b05ab39c8ac
20243ds064@utez.edu.mx <20243ds064@utez.edu.mx>
2025-05-29 09:42:24 -0600

    se crea el script usuarioSeguro

--- U1_TI_05_UsusarioContraseñaSegura/src/Model/Main.java
+++ U1_TI_05_UsusarioContraseñaSegura/src/Model/Main.java
@@ -1,5 +1,30 @@
 package Model;
+import javax.swing.JOptionPane;
 
 public class Main {
+    public static void main(String[] args) throws Exception {
+
+         String usuario = JOptionPane.showInputDialog("Ingrese su nombre de usuario:");
+        String password = JOptionPane.showInputDialog("Ingrese su contraseña:");
+
+        UsuarioSeguro usuarioSeguro = new UsuarioSeguro();
+        usuarioSeguro.setNombreUsuario(usuario);
+        usuarioSeguro.setPassword(password);
+
+        if (usuarioSeguro.autenticar(password)) {
+            System.out.println("Usuario inicializado correctamente.");
+        } else {
+            System.out.println("Error al inicializar el usuario.");
+            return;
+        }
+
+        boolean acceso = usuarioSeguro.autenticar(password);
+        if (acceso){
+            JOptionPane.showMessageDialog(null, "Acceso concedido");
+
+        }else{
+            JOptionPane.showMessageDialog(null, "Acceso denegado");
+        }
+    }
 
 }
--- U1_TI_05_UsusarioContraseñaSegura/src/Model/UsuarioSeguro.java
+++ U1_TI_05_UsusarioContraseñaSegura/src/Model/UsuarioSeguro.java
@@ -12,11 +12,30 @@ public class UsuarioSeguro {
     }
 
     public void setPassword(String password){
-        if(password.length() < 8 || password == null){
-           System.out.println("la contraseña debe tener al menos 8  caracteres");
-        } 
-        
+        if(password.length() > 8 && password != null){
+           //System.out.println("la contraseña debe tener al menos 8  caracteres");
+        }
+        for(int i = 0; i < password.length(); i++){
+            char c = password.charAt(i); 
+            if(Character.isUpperCase(c)==false){
+             System.out.println("la contraseña debe tener al menos una mayuscula");
+            }else if(Character.isLowerCase(c) == false){
+             System.out.println("la contraseña debe tener al menos una minuscula");
+            }else if(Character.isDigit(c)==false){
+             System.out.println("la contraseña debe tener al menos un digito");
+            }
+            
+            
+        }
+         this.password=password; 
+        }
 
+        public boolean autenticar(String intentPassword){
+            if(intentPassword.equals(password)){
+               return true;
+            }else{
+                return false;
+            }
         }
     }
 
